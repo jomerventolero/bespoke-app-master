eedabba89774391ab0874758c02542c9e7b99fae
orekihoutaro <orekihoutaro1218@gmail.com>
2023-02-20 16:59:31 +0800

    Added Animations

--- src/components/HowitWorks.jsx
+++ src/components/HowitWorks.jsx
@@ -10,21 +10,29 @@ import { motion } from 'framer-motion'
 const HowitWorks = () => {
   return (
     <div id="howitworks">
-        <motion.div className="relative flex flex-col justify center px-10"
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+        <motion.div className="relative flex flex-col justify center px-10 py-8"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{
             duration: 0.8,
             delay: 0.2,
             ease: [0, 0.71, 0.2, 1.01]
             }}  
             >
-            <img src={ howitworks } alt="howitworks" className="w-3/4 justify-center self-center" />
-            <div className="absolute top-[18%] left-1/4 transform -translate-x-1/4  text-brandBlack text-2xl p-5 font-poppins ">
+            <img src={ howitworks } alt="howitworks" className="blur-[1px] w-3/4 justify-center self-center" />
+            <motion.div className="absolute top-[18%] left-1/4 transform -translate-x-1/4  text-brandBlack text-2xl p-5 font-poppins"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 2.8,
+              delay: 0.4,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+            >
                 <h1>Outsource to the Philippines</h1>
                 <span className="text-blue-400">and create a dedicated team,</span>
                 <h1>without the worry about office space, IT,<br/> recruitment, payroll, and more.</h1>
-            </div>
+            </motion.div>
         </motion.div>
         <motion.div className="flex flex-row gap-4 px-8 py-10 overflow-hidden justify-center">
             <motion.img src={one} alt="one" className="w-[20%] h-[20%]" 
@@ -41,7 +49,7 @@ const HowitWorks = () => {
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
                 duration: 0.8,
-                delay: 0.2,
+                delay: 0.3,
                 ease: [0, 0.71, 0.2, 1.01]
               }}  
             />
@@ -50,7 +58,7 @@ const HowitWorks = () => {
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
                 duration: 0.8,
-                delay: 0.2,
+                delay: 0.4,
                 ease: [0, 0.71, 0.2, 1.01]
               }}  
             />
@@ -59,18 +67,18 @@ const HowitWorks = () => {
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
                 duration: 0.8,
-                delay: 0.2,
+                delay: 0.5,
                 ease: [0, 0.71, 0.2, 1.01]
               }}  
             />
         </motion.div>
         <div className="flex flex-col  overflow-hidden px-[15%] pb-4">
           <motion.div className="w-full h-full px-8 justify-center bg-gradient-to-t from-blue-300 to-blue-200"
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{
-            duration: 0.8,
-            delay: 0.2,
+            duration: 1.8,
+            delay: 0.6,
             ease: [0, 0.71, 0.2, 1.01]
           }}  
           >
