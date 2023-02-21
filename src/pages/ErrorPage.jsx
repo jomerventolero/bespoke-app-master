9230eae520b55db5149ce429fabe77d2c99db37a
orekihoutaro <orekihoutaro1218@gmail.com>
2023-02-21 15:23:22 +0800

    Added an Error 404 Page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { motion } from 'framer-motion'
+import errorPage from '../assets/404.png'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <motion.div className="flex justify-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+                duration: 4.5,
+                delay: 0.2
+            }}
+        >
+            <img src={ errorPage } alt="404" className='w-full h-full'/>
+        </motion.div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default ErrorPage
