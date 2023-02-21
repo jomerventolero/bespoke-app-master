bcff987e5b2904098e67b4e0f2c5dd2d22482440
orekihoutaro <orekihoutaro1218@gmail.com>
2023-02-21 14:30:51 +0800

    Changed phonenumber

--- src/components/WhatsAppFloatingButton.jsx
+++ src/components/WhatsAppFloatingButton.jsx
@@ -5,6 +5,6 @@ import logo from '../assets/logo.png'
 export default function App() {
 
   return (
-      <FloatingWhatsApp phoneNumber="639760976171" accountName='BeSpoke' statusMessage='Typically replies within 1 hour' avatar={ logo } />
+      <FloatingWhatsApp phoneNumber="639472960890" accountName='BeSpoke' statusMessage='Typically replies within 1 hour' avatar={ logo } />
   )
 }
