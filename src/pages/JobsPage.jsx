c07c0e0dec822e6f9dadfbb3f565146ba55b441e
orekihoutaro <orekihoutaro1218@gmail.com>
2023-02-21 15:23:06 +0800

    Edited Jobs

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -7,16 +7,17 @@ const JobsPage = () => {
   return (
     <div className="flex flex-col gap-4">
         <Navbar />
-     <motion.div className="flex flex-col justify-center font-poppins font-bold text-[80px] text-brandBlue text-center py-[15%]"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ 
-            duration: 2.5,
-            delay: 0.5
-        }}
-     >Coming Soon...
-     </motion.div>
-        <Footer />
+      <motion.div className="flex flex-col justify-center font-poppins font-bold text-[80px] text-brandBlue text-center py-[15%]"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ 
+              duration: 2.5,
+              delay: 0.5
+          }}
+      >Contact us at<br/>
+      info@mybespokestaff.com
+      </motion.div>
+      <Footer />
     </div>
   )
 }
