1bec09ba08b3c7bccae845dce296cfafad9d0995
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-03-01 11:47:25 +0800

    Change text leading styles

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import styles from '../style'
 
-const CustomButton = ({ name, icon, seperator, redirect, tpx}) => {
+const CustomButton = ({ name, icon, seperator, redirect}) => {
   return (
     <motion.div 
       initial = {{ opacity: 0 }}
@@ -11,11 +12,11 @@ const CustomButton = ({ name, icon, seperator, redirect, tpx}) => {
         delay: 0.4
        }}
 
-      className="w-[50%] rounded-full bg-brandBlack hover:bg-brandBlue pr-4 pl-10">
+      className="w-[50%] rounded-full bg-brandBlack hover:bg-brandBlue pr-2 pl-2">
         <a href={redirect} className="flex flex-row items-center justify-evenly">
-            <img src={icon} alt="icon" className="w-[120px] h-[120px]"/>
-            <img src={seperator} alt="icon" className="w-[64px] h-[148px] p-6"/>
-            <h1 className={`text-white text-[${tpx}px] xl:text-[38px] lg:text-[35px] md:text-[26px] sm:text-[20px] text-center leading-10 `}>
+            <img src={icon} alt="icon" className="w-[18%] h-auto"/>
+            <img src={seperator} alt="icon" className="w-[10%] h-[100%] p-4"/>
+            <h1 className={`text-white ${ styles.textResHeadings} text-center leading-6 `}>
                 { name }
             </h1>
         </a>
