dc7797061ef21031e034ee7ea2deb1cc2f60c869
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-03-01 11:50:08 +0800

    Responsiveness Update

--- src/components/WhyChooseBespoke.jsx
+++ src/components/WhyChooseBespoke.jsx
@@ -1,9 +1,14 @@
 import bespokeProfile from '../assets/bespoke-profile.png'
+import styles from '../style'
 
 const WhyChooseBespoke = () => {
   return (
-    <div>
-      <img src={bespokeProfile} alt="Bespoke" className="w-full"/>
+    <div classNAme="px-[10%]">
+      <div className={`flex flex-row justify-center bg-gradient-to-b from-violet-500 to-indigo-400 font-poppins font-semibold py-[30px] gap-4 my-2`}>
+        <span className={`${styles.textResHeadline} text-white`}>WHY CHOOSE</span>
+        <span className={`${styles.textResHeadline} text-brandBlack`}>BESPOKE?</span>
+      </div>
+      <img src={bespokeProfile} alt="Bespoke" draggable="false" className="w-screen"/>
     </div>
   )
 }
