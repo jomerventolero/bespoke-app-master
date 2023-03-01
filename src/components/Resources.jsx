cdab42cfd6d41138d98939a4c4909552ae656eb5
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-03-01 11:50:30 +0800

    Content Update

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -5,10 +5,10 @@ import styles from '../style'
 const Resources = () => {
   return (
     <section id="resources">
-      <div className="flex flex-col ">
+      <div className="flex flex-col pt-[8%]">
         <div className="flex flex-col">
-          <img src={ whydofreightforwarding } alt="whydofreightforwarding" className="p-[40px]"/>
-          <h1 className={`absolute top-[15%] lg:hidden font-poppins font-semibold text-left p-4 justify-end ${styles.textResTitle}`}>WHY DO FREIGHT FORWARDING COMPANIES USE BPO SERVICES</h1>
+          <img src={ whydofreightforwarding } alt="whydofreightforwarding" className="xs:block hidden p-[40px]"/>
+          <h1 className={`lg:hidden font-poppins font-semibold text-left p-4 px-[6%] justify-end ${styles.textResTitle}`}>WHY DO FREIGHT FORWARDING COMPANIES USE BPO SERVICES</h1>
         </div>
       </div>
     </section>
