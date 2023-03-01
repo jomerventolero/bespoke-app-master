f408d5818fa739c320a6e0c8cd9688cef4effdc7
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-03-01 11:45:11 +0800

    Responsiveness Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import WhatsAppFloatingButton from './components/WhatsAppFloatingButton'
-import styles from './style'
 import Footer from './components/Footer'
 import Testimonials from './components/Testimonials'
 import WhyChooseBespoke from './components/WhyChooseBespoke'
@@ -13,26 +12,28 @@ function App() {
 
   return (
     <div className="bg-white">
-      <div className={`${styles.flexCenter}`}>
+      <div>
         <Navbar />
       </div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 5.8,
-          delay: 0.2,
-          ease: [0, 0.71, 0.2, 1.01]
-        }}  
-      >
-        <ParallaxHero/>
-      </motion.div>
-      <div>
-        <WhyChooseBespoke/>
-        <Testimonials />
-        <Footer />
-      </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 5.8,
+            delay: 0.2,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}  
+          className="absolute top-[10%]"
+        >
+          <ParallaxHero/>
+          <div className="px-[10%]">
+            <WhyChooseBespoke/>
+            <Testimonials />
+          </div>
+          <Footer />
+        </motion.div>
       <WhatsAppFloatingButton />
+
     </div>
   )
 }
