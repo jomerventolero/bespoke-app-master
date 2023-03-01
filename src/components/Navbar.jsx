3196b5824e149d1bdec0018dd2c67f3066d293aa
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-03-01 11:48:19 +0800

    Added some fix z-index and fixed the Navbar to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <nav className={`w-full bg-white top-0 flex shadow-xl py-1 px-6 justify-start items-center navbar`}>
+    <nav className={`z-50 fixed w-full bg-white top-0 flex shadow-xl py-1 px-6 justify-start items-center navbar`}>
         <a href='/'>
             <img src={ logo } alt="logo" className={`object-contain block w-[35%] cursor-pointer ${toggle ? 'w-[100px]' : 'w-[35%]'}`}/>
         </a>
@@ -22,7 +22,7 @@ const Navbar = () => {
                     key={nav.id}
                     className={`font-poppins font-bold cursor-pointer justify-between place-items-start text-[20px] text-black ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
                 >
-                    <a href={`${nav.id}`} className={`hover:text-brandBlue text-center max-h-4 ${styles.textResNavLinks} `}>
+                    <a href={`${nav.id}`} className={`hover:text-brandBlue text-center text-brandBlack max-h-4 ${styles.textResNavLinks} `}>
                         {nav.title}
                     </a>
                 </li>
@@ -35,7 +35,7 @@ const Navbar = () => {
 
         <div className='md:hidden flex flex-1 justify-end items-center '>
           <Hamburger toggled={ toggle } toggle={ setToggle } />
-          <div className={`${toggle ? 'flex' : 'hidden'} float-right top-[12%] right-[-10px] bg-white mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <div className={`${toggle ? 'flex' : 'hidden'} absolute float-right top-[12%] right-[-10px] bg-white mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
             <ul className="flex flex-1 flex-col justify-end items-center  ">
               {navLinks.map((nav, index) => (
                 <li
