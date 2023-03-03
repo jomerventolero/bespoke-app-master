41bdf3d7ac165e8d42ead264bd4e5c0c8ec097b7
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-03-03 09:00:35 +0800

    Added SEO

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
   return (
     <div className="bg-white">
       <SuperSEO
-        title="Bespoke"
-        description="Bespoke is a full-service digital agency that helps businesses grow online. We are a team of passionate designers, developers, and marketers who are committed to delivering results."
-        url="https://mybespokestaff.com/"
+        title="Bespoke Enterprise Solutions Inc. | My Bespoke Staff."
+        description="Bespoke is led by a team of professionals that have at least 20 years combined experience in the freight forwarding industry in Australia. We are well versed with the importation and exportation requirements that are unique to Australia. Be it the rules and regulations of AQIS (Australian Quarantine and Inspection Scheme) or the daily operations of the various departments such as the operations / forwarding team to the cartage department and the customs broker department, we have done it all. Do drop us a quick enquiry of how we can be of service to you on your staffing requirements. We are certain we would even be able to advise you on building a team of staff that can generate more income for your business that you didn't know you needed."
+        url="https://www.mybespokestaff.com/"
       />
       <Navbar />
       <motion.div
