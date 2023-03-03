a8f9107d67dac46cc57aa3d8c361dde35bb112a8
orekihoutaro <orekihoutaro1218@gmail.com>
2023-03-03 10:40:52 +0800

    Lazy Loading Update

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <Suspense fallback={<Loader/>}>
-      <div className="bg-white">
+      <div className="bg-white overflow-hidden">
         <SuperSEO
           title="Bespoke Enterprise Solutions Inc. | My Bespoke Staff."
           description="Bespoke is led by a team of professionals that have at least 20 years combined experience in the freight forwarding industry in Australia. We are well versed with the importation and exportation requirements that are unique to Australia. Be it the rules and regulations of AQIS (Australian Quarantine and Inspection Scheme) or the daily operations of the various departments such as the operations / forwarding team to the cartage department and the customs broker department, we have done it all. Do drop us a quick enquiry of how we can be of service to you on your staffing requirements. We are certain we would even be able to advise you on building a team of staff that can generate more income for your business that you didn't know you needed."
