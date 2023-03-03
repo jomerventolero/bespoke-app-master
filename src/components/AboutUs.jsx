b2fdeca05be3fc1fdfd03d3f3b15ee4d3a39149b
orekihoutaro <orekihoutaro1218@gmail.com>
2023-03-03 17:22:53 +0800

    Added Block Gradients

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import aboutus from '../assets/aboutus.png'
-import gradientbox from '../assets/gradient-box.png'
 import aboutusmobile from '../assets/aboutusmobile.png'
 import aboutusmobile1 from '../assets/aboutusmobile1.png'
 import { motion } from 'framer-motion'
@@ -28,24 +27,33 @@ const AboutUs = () => {
             </span>    
         </motion.div>
         <div className="flex flex-row px-[10%]  py-4">
-            <img src={gradientbox} alt="box" className="w-[40%] h-auto xs:block hidden"/>
+            <div className="w-[480px] h-[300px] bg-gradient-to-t from-indigo-500 to-blue-500 rounded-l-lg xs:block hidden">
+            </div>
             <div className="flex flex-col justify-center text-center">
                 <h1 className="font-poppins font-semibold text-brandBlue text-[30px] py-2">BESPOKE MISSION</h1>
                 <span className="px-[20%]">We work towards operating in a manner where our client's are satisfied that their business processes are met with the highest standard of service and effort by the top class talents that we hire.</span>
             </div>
+            <div className="w-[480px] h-[300px] bg-gradient-to-t from-indigo-500 to-blue-500 rounded-r-lg xs:block hidden">
+            </div>
         </div>
         <div className="flex flex-row px-[10%] py-4">
+        <div className="w-[480px] h-[300px] bg-gradient-to-t from-indigo-500 to-blue-500 rounded-l-lg xs:block hidden">
+            </div>
             <div className="flex flex-col justify-center text-center">
                 <h1 className="font-poppins font-semibold text-brandBlue text-[30px] py-2">BESPOKE VISION</h1>
                 <span className="px-[20%]">We believe in quality over quantity. Therefore we ensure that we get the best person to do the job right with no expense spared on the constant upgrade of workforce skills, technology and productivity.</span>
             </div>
-            <img src={gradientbox} alt="box" className="xs:block hidden w-[40%] h-auto transform rotate-180"/>
+            <div className="w-[480px] h-[300px] bg-gradient-to-t from-indigo-500 to-blue-500 rounded-r-lg xs:block hidden">
+            </div>
         </div>
-        <div className="flex flex-row px-[10%] py-4">
-            <img src={gradientbox} alt="box" className="xs:block hidden w-[40%] h-auto"/>
+            <div className="flex flex-row px-[10%] py-4">
+            <div className="w-[480px] h-[300px] bg-gradient-to-t from-indigo-500 to-blue-500 rounded-l-lg xs:block hidden">
+            </div>
             <div className="flex flex-col justify-center text-center">
                 <h1 className="font-poppins font-semibold text-brandBlue text-[30px] py-2">BESPOKE CORE VALUES</h1>
                 <span className="px-[20%]">We strive to maintain our integrity whenever it comes to fairness in the workplace, stellar productivity on the services we provide and securing the data that we process on behalf of our customers.</span>
+            </div>
+            <div className="w-[480px] h-[300px] bg-gradient-to-t from-indigo-500 to-blue-500 rounded-r-lg xs:block hidden">
             </div>
         </div>
     </div>
