2de89df5265ba3a2e8a4d920741e6f0e68870ca8
orekihoutaro <orekihoutaro1218@gmail.com>
2024-01-27 11:35:29 +0800

    Submenu

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -10,8 +10,9 @@ const SubMenu = () => {
     <div className="">
         <FaBars size="2rem" onClick={() => setToggle(!toggle)} className={`cursor-pointer ${toggle ? 'hidden' : 'block'}`}/>
         <IoMdCloseCircleOutline size="2rem" onClick={() => setToggle(!toggle)} className={`cursor-pointer ${toggle ? 'block' : 'hidden'}`}/>
-        <motion.div className={`${toggle ? "block" : "hidden"}`}>
-
+        <motion.div className={`absolute top-28 right-8 flex flex-col gap-4 items-center text-xl font-semibold bg-white rounded-lg p-4 ${toggle ? "block" : "hidden"}`}>
+            <a href="https://odoo.mybespokestaff.com/web/login">BES Login</a>
+            <a href="/contactus">Contact Us</a>
         </motion.div>
     </div>
   )
