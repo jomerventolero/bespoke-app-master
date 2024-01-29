c636390f4fded4724e25844ce79186c3bbf7dd62
orekihoutaro <orekihoutaro1218@gmail.com>
2024-01-29 14:25:19 +0800

    SubMenu Added

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -10,9 +10,14 @@ const SubMenu = () => {
     <div className="">
         <FaBars size="2rem" onClick={() => setToggle(!toggle)} className={`cursor-pointer ${toggle ? 'hidden' : 'block'}`}/>
         <IoMdCloseCircleOutline size="2rem" onClick={() => setToggle(!toggle)} className={`cursor-pointer ${toggle ? 'block' : 'hidden'}`}/>
-        <motion.div className={`absolute top-28 right-8 flex flex-col gap-4 items-center text-xl font-semibold bg-white rounded-lg p-4 ${toggle ? "block" : "hidden"}`}>
-            <a href="https://odoo.mybespokestaff.com/web/login">BES Login</a>
-            <a href="/contactus">Contact Us</a>
+        <motion.div 
+            initial={{ opacity: 0, y: -100}}
+            whileInView={{ opacity: 1, y: 0}}
+            transition={{ duration: 0.5, type: "fadeIn"}}
+            exit={{ opacity: 0, y: -100}}
+            className={`absolute top-28 right-8 flex flex-col gap-4 w-36 items-center text-xl font-semibold bg-white rounded-lg p-4 ${toggle ? "block" : "hidden"}`}>
+            <a href="https://odoo.mybespokestaff.com/web/login" className='hover:text-brandBlue'>BES Login</a>
+            <a href="/contactus" className='hover:text-brandBlue'>Contact Us</a>
         </motion.div>
     </div>
   )
